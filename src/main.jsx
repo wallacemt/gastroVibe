8491c7250365fa23dbb1cb086dcc9a854709c9b1
Wallace Santana <wallacesantanak0@gmail.com>
2024-11-30 12:05:13 -0300

    add vercel/analytics

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import { RouteApp } from './routes'
+import {Analytics} from '@vercel/analytics/react'
+
+
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouteApp/>
+    <Analytics/>
   </StrictMode>,
 )
